87b023733e419d2d300da16171193fb8ef42f9f4
mp4504 <pengms0827@gmail.com>
2017-02-24 23:00:10 -0500

    ios app basic ui and flow
    ios app basic ui and flow

---
+++ Pi-Spy-iOS/Pi-Spy/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Pi-Spy
+//
+//  Created by 彭铭仕 on 2/23/17.
+//  Copyright © 2017 Home Surveillance Inc. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
