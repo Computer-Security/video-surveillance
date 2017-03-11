4fcb2f301359fe47ed17906aec8a332d3728d735
mp4504 <pengms0827@gmail.com>
2017-03-11 15:08:22 -0500

    Date Picker UI updated
    Updated History tab date picker UI.

--- Pi-Spy-iOS/Pi-Spy/FirstViewController.swift
+++ Pi-Spy-iOS/Pi-Spy/FirstViewController.swift
@@ -9,17 +9,42 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+  
+  @IBOutlet weak var datePickerTxt: UITextField!
+  let datePicker = UIDatePicker();
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    createDatePicker()
+  }
 
 
+  
+  func createDatePicker(){
+    // toolbar
+    let toolbar = UIToolbar();
+    toolbar.sizeToFit();
+    
+    // bar button item
+    let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(donePressed))
+    toolbar.setItems([doneButton], animated: false)
+    
+    datePickerTxt.inputAccessoryView = toolbar
+    
+    // assigning date picker to text field
+    datePicker.datePickerMode = .date
+    datePickerTxt.inputView = datePicker
+  }
+  
+  func donePressed(){
+    
+    // format date
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateStyle = .short
+    dateFormatter.timeStyle = .none
+    
+    datePickerTxt.text = dateFormatter.string(from: datePicker.date)
+    self.view.endEditing(true);
+  }
 }
 
