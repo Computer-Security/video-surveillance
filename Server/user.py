db9a5bc9022f89ff96679e32b49d0dbcc6bee562
Mi Fang <fangm1994@gmail.com>
2017-04-06 15:54:07 -0400

    Add user class

---
+++ Server/user.py
@@ -0,0 +1,20 @@
+from flask.ext.login import UserMixin
+
+class User(UserMixin):
+
+    def __init__(self, uid):
+        self.id = uid
+        self.name = ''
+   	
+   	def get_name(self):
+   		return self.name   
+   		 
+    def set_name(self, name):
+    	self.name = name
+        
+    def __repr__(self):
+        return "%d/%s" % (self.id, self.name)
+
+    def get_id(self):
+    	return unicode(self.id)
+    	# return unicode(self.session_token)
